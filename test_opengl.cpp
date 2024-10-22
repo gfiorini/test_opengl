11ccf82b24a3b6e5f0b87979fdb0e428d320cadb
Gian <gianluca.fiorinigmail.com>
2024-10-22 22:05:02 +0200

    init

---
+++ test_opengl.cpp
@@ -0,0 +1,12 @@
+﻿// test_opengl.cpp : Defines the entry point for the application.
+//
+
+#include "test_opengl.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
