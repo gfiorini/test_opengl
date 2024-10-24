9aa44d57322e82ad42534feb72d7675e13d08cf9
gfiorini <gianluca.fiorini@gmail.com>
2024-10-25 00:18:28 +0200

    triangle without shader

--- src/test_opengl.cpp
+++ src/test_opengl.cpp
@@ -1,52 +1,22 @@
 ﻿#include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include "linmath.h"
+//#include "linmath.h"
 #include <iostream>
 
-typedef struct Vertex
-{
-    vec2 pos;
-    vec3 col;
-} Vertex;
+// typedef struct Vertex
+// {
+//     vec2 pos;
+//     vec3 col;
+// } Vertex;
+//
+// static const Vertex vertices[3] =
+// {
+//     { { -0.6f, -0.4f }, { 1.f, 0.f, 0.f } },
+//     { {  0.6f, -0.4f }, { 0.f, 1.f, 0.f } },
+//     { {   0.f,  0.6f }, { 0.f, 0.f, 1.f } }
+// };
 
-static const Vertex vertices[3] =
-{
-    { { -0.6f, -0.4f }, { 1.f, 0.f, 0.f } },
-    { {  0.6f, -0.4f }, { 0.f, 1.f, 0.f } },
-    { {   0.f,  0.6f }, { 0.f, 0.f, 1.f } }
-};
 
-static const char* vertex_shader_text =
-"#version 330\n"
-"uniform mat4 MVP;\n"
-"in vec3 vCol;\n"
-"in vec2 vPos;\n"
-"out vec3 color;\n"
-"void main()\n"
-"{\n"
-"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
-"    color = vCol;\n"
-"}\n";
-
-static const char* fragment_shader_text =
-"#version 330\n"
-"in vec3 color;\n"
-"out vec4 fragment;\n"
-"void main()\n"
-"{\n"
-"    fragment = vec4(color, 1.0);\n"
-"}\n";
-
-static void error_callback(int error, const char* description)
-{
-    fprintf(stderr, "Error: %s\n", description);
-}
-
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
-}
 
 int main(void)
 {
@@ -79,9 +49,34 @@ int main(void)
     fprintf(stdout, "GLEW Version: %s\n", glewGetString(GLEW_VERSION));
     fprintf(stdout, "OpenGl Version / Driver version: %s\n", glGetString(GL_VERSION));
 
+
+    // vertix with pos and uvmap
+    float vertices[] = {
+         -0.5f, -0.5f, 0, 0,
+          0.5f, -0.5f, 0, 0,
+          0.0f,  0.5f, 0, 0,
+    };
+
+    constexpr GLsizei n = 1;
+    GLuint vertexBuffer;
+
+    // create buffer to store vertexes
+    glGenBuffers(n, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+
+    // copy vertexes in buffer (in graphic card)
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // size = num element in index (position = 2 elements, uv = 2 elements)
+    // stride = size in bytes of vertex 2*2 float (position) + 2*2 float (uv coords)
+    // pointer = offest size, position = 0, uvmap = 8 = 2 * 4bytes (1 float = 32bit = 4 bytes)
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
