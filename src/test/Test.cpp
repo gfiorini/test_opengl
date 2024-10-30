a12a8eec1fe91ad139581af0f2d758b43d13eb3d
gfiorini <gianluca.fiorini@gmail.com>
2024-10-30 22:40:02 +0100

    refactor + testcase

---
+++ src/test/Test.cpp
@@ -0,0 +1,73 @@
+//
+// Created by gfiorini on 10/30/24.
+//
+
+#include "Test.h"
+
+#include <cstring>
+#include <iostream>
+#include <ostream>
+
+void test1() {
+    std::cout << "sizeof(unsigned char): " << sizeof(unsigned char) << std::endl;
+    std::cout << "sizeof(char): " << sizeof(char) << std::endl;
+    std::cout << "sizeof(short int): " << sizeof(short int) << std::endl;
+    std::cout << "sizeof(short int): " << sizeof(short int) << std::endl;
+    std::cout << "sizeof(int): " << sizeof(int) << std::endl;
+    std::cout << "sizeof(long): " << sizeof(long) << std::endl;
+    std::cout << "sizeof(long long): " << sizeof(long long) << std::endl;
+    std::cout << "sizeof(float) " << sizeof(float) << std::endl;
+    std::cout << "sizeof(double): " << sizeof(double) << std::endl;
+    std::cout << "sizeof(unsigned int): " << sizeof(unsigned int) << std::endl;
+
+}
+
+
+
+
+
+void test2() {
+    int r = strcmp("abcd", "abcd"); //return 0 if equal
+    if (r) {
+        std::cout << "strcmp(" << __FILE__ << ", " << __LINE__ << ") " << r << std::endl;
+    } else {
+
+    }
+
+}
+
+void testMemory() {
+    long long size = 8LL * 1024 * 1024 * 1024; // 8 GB
+
+    char* buffer = (char*)malloc(size); // Allocate memory
+
+    if (buffer == NULL) {
+        printf("Memory allocation failed.\n");
+        return;
+    }
+
+    // Fill the buffer with data
+    memset(buffer, 'A', size); // Fill with 'A'
+
+    printf("Buffer filled. Press Enter to free memory and exit.\n");
+    getchar(); // Wait for user input
+
+    free(buffer); // Free allocated memory
+
+}
+
+
+
+void callFunction(int(* aaa)(const char *x, const char *y)) {
+    aaa("hello", "world");
+}
+
+int main() {
+
+    testMemory();
+
+    std::cout << "fine" << std::endl;
+
+}
+
+
---
+++ src/test/Test.h
@@ -0,0 +1,8 @@
+//
+// Created by gfiorini on 10/30/24.
+//
+
+#ifndef TEST_H
+#define TEST_H
+
+#endif //TEST_H
