65d16af19bc3c029bc2b861c4c0fa1a701441650
Gian <gianluca.fiorinigmail.com>
2024-10-31 19:01:55 +0100

    MSVC compatibiliy for static_assert(false)

--- src/main/VertexBufferLayout.cpp
+++ src/main/VertexBufferLayout.cpp
@@ -2,11 +2,16 @@
 // Created by gfiorini on 10/31/24.
 //
 
+#include <stdexcept>
 #include "VertexBufferLayout.h"
 
 template<typename T>
 void VertexBufferLayout::Push(int count) {
+#ifdef _MSC_VER  // MSVC
+    throw std::runtime_error("Unsupported operation VertexBufferLayout::Push");
+#else  // GCC or Clang
     static_assert(false);
+#endif
 }
 
 template<>
